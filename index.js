da7ba2f58a7576002f6666e3a7b69e5fc26f290d
Haim Rubin <haim.rubin@gmail.com>
2021-04-21 19:31:53 +0300

    initial vesrion

---
+++ index.js
@@ -0,0 +1,21 @@
+const sleep = (seconds, value) => {
+    return new Promise (resolve => {
+        setTimeout(() => {
+            resolve(value)
+        }, seconds * 1000)
+    })
+}
+
+
+const run = async () => {
+
+    console.log('before')
+
+    const res = await sleep(2, 100)
+
+    console.log(res)
+    console.log('after')
+
+}
+
+run()
